4b5b46d91609aad435970f25db35ed571395c4bb
dcheris <97857929+dcheris@users.noreply.github.com>
2022-11-12 17:48:45 -0600

    making changes to the routing file. Will test how to route to html files.

--- routing.js
+++ routing.js
@@ -1,59 +1,13 @@
-const http = require("http")
-const fs = require("fs")
-//const router = require("router")
+const http = require('http');
+const server = http.createServer(function(req, res){
 
+if (req.url == '/home' && req.method == 'GET'){
+    res.write("This is the home page");
+    res.end();
 
-
-const server = http.createServer(function(req, res) {
-
-    if (req.url == '/home'  && req.method == "GET"){
-        res.writeHead(200, {"Content-type": "text/html; charset=utf-8"})
-        fs.readFile("home.html",function(err, data) {
-            // console.log("Uh oh")
-        res.write(data)
-        res.end()
-    })
-    }else if (req.url == '/bookmarks'  && req.method == "GET") {
-        res.writeHead(200, {"Content-type": "text/html; charset=utf-8"})
-        fs.readFile("bookmarks.html", function(err, data) {
-        res.write(data)
-        res.end()
-    })
-    }else if (req.url == '/favorites'  && req.method == "GET") {
-        res.writeHead(200, {"Content-type": "text/html; charset=utf-8"})
-        fs.readFile("favorites.html", function(err, data) {
-        res.write(data)
-        res.end()
-    })
-    }else if 
-        (req.url == '/all'  && req.method == "GET") {
-        res.writeHead(200, {"Content-type": "text/html; charset=utf-8"})
-        fs.readFile("all.html", function(err, data) {
-        res.write(data)
-        res.end()
-    })
-
-   }else {
-    res.writeHead(200, {"Content-type": "text/html; charset=utf-8"})
-        fs.readFile("home.html", function(err, data) {
-        res.write(data)
-        res.end()
-    })
+        }else{
+        res.write("Homepage");
+        res.end();
     }
-//if (err.code ---)
 
-}).listen(3000)
-
-
-//Attempting to structure a callback error
-
-function errorFirstCallback(err, data) {
-  if (err) {
-    console.error('There was an error', err);
-    return;
-  }
-  console.log(data);
-}
-
-fs.readFile('', errorFirstCallback);
-fs.readFile('/some/file/that/does-exist', errorFirstCallback);
+}).listen(3000);
