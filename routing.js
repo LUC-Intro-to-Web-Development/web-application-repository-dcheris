67ec4cdf34fade4abfbbdd9f1feed5beab7cafb5
dcheris <97857929+dcheris@users.noreply.github.com>
2022-11-11 20:07:42 -0600

    adding some HTML and CSS elements, correcting routing.js.

--- routing.js
+++ routing.js
@@ -4,21 +4,40 @@ const fs = require("fs")
 
 const server = http.createServer(function(req, res) {
 
-    if (req.URL == '/home'  && req.method == GET) {
-        res.write("Home page")
+    if (req.url == '/home'  && req.method == "GET"){
+        res.writeHead(200, {"Content-type": "text/html"})
+        fs.readFile("home.html",function(err, data) {
+            // console.log("Uh oh")
+        res.write(data)
         res.end()
-    }
-    else if (req.URL == '/bookmarks'  && req.method == GET) {
-        res.write("Bookmarks")
+    })
+    }else if (req.url == '/bookmarks'  && req.method == "GET") {
+        res.writeHead(200, {"Content-type": "text/html"})
+        fs.readFile("bookmarks.html", function(err, data) {
+        res.write(data)
         res.end()
-    }
-    else if (req.URL == '/favorites'  && req.method == GET) {
-        res.write("Favorites")
+    })
+    }else if (req.url == '/favorites'  && req.method == "GET") {
+        res.writeHead(200, {"Content-type": "text/html"})
+        fs.readFile("favorites.html", function(err, data) {
+        res.write(data)
         res.end()
-    }
-    else { (req.URL == '/all'  && req.method == GET) 
-        res.write("All")
+    })
+    }else if 
+        (req.url == '/all'  && req.method == "GET") {
+        res.writeHead(200, {"Content-type": "text/html"})
+        fs.readFile("all.html", function(err, data) {
+        res.write(data)
         res.end()
-    }
+    })
 
-}) .listen(3000)
+   }else {
+    res.writeHead(200, {"Content-type": "text/html"})
+        fs.readFile("home.html", function(err, data) {
+        res.write(data)
+        res.end()
+    })
+    }
+if (err.code ---)
+
+}).listen(3000)
